0c1761ecddf9ff822920d25d2a049bbb5d971b7a
reinaldoferraz <reinaldo@nic.br>
2016-10-25 10:12:16 -0200

    adding new circle feature

--- script.js
+++ script.js
@@ -61,81 +61,116 @@ var allinternet;
 }
 
 
-//acao para as perguntas de quando executou ação na Web - evento
+/*
+ * Konami-JS ~ 
+ * :: Now with support for touch events and multiple instances for 
+ * :: those situations that call for multiple easter eggs!
+ * Code: https://github.com/snaptortoise/konami-js
+ * Examples: http://www.snaptortoise.com/konami-js
+ * Copyright (c) 2009 George Mandis (georgemandis.com, snaptortoise.com)
+ * Version: 1.4.6 (3/2/2016)
+ * Licensed under the MIT License (http://opensource.org/licenses/MIT)
+ * Tested in: Safari 4+, Google Chrome 4+, Firefox 3+, IE7+, Mobile Safari 2.2.1 and Dolphin Browser
+ */
 
-function fiznaweb() {
-    var segue = document.getElementsByClassName("previous");
-    var hide;
-    for (hide = 0; hide < segue.length; hide++) {
-        segue[hide].style.display = "none";  
-        
+var Konami = function (callback) {
+    var konami = {
+        addEvent: function (obj, type, fn, ref_obj) {
+            if (obj.addEventListener)
+                obj.addEventListener(type, fn, false);
+            else if (obj.attachEvent) {
+                // IE
+                obj["e" + type + fn] = fn;
+                obj[type + fn] = function () {
+                    obj["e" + type + fn](window.event, ref_obj);
+                }
+                obj.attachEvent("on" + type, obj[type + fn]);
+            }
+        },
+        input: "",
+        pattern: "38384040373937396665",
+        load: function (link) {
+            this.addEvent(document, "keydown", function (e, ref_obj) {
+                if (ref_obj) konami = ref_obj; // IE
+                konami.input += e ? e.keyCode : event.keyCode;
+                if (konami.input.length > konami.pattern.length)
+                    konami.input = konami.input.substr((konami.input.length - konami.pattern.length));
+                if (konami.input == konami.pattern) {
+                    konami.code(link);
+                    konami.input = "";
+                    e.preventDefault();
+                    return false;
+                }
+            }, this);
+            this.iphone.load(link);
+        },
+        code: function (link) {
+            window.location = link
+        },
+        iphone: {
+            start_x: 0,
+            start_y: 0,
+            stop_x: 0,
+            stop_y: 0,
+            tap: false,
+            capture: false,
+            orig_keys: "",
+            keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
+            code: function (link) {
+                konami.code(link);
+            },
+            load: function (link) {
+                this.orig_keys = this.keys;
+                konami.addEvent(document, "touchmove", function (e) {
+                    if (e.touches.length == 1 && konami.iphone.capture == true) {
+                        var touch = e.touches[0];
+                        konami.iphone.stop_x = touch.pageX;
+                        konami.iphone.stop_y = touch.pageY;
+                        konami.iphone.tap = false;
+                        konami.iphone.capture = false;
+                        konami.iphone.check_direction();
+                    }
+                });
+                konami.addEvent(document, "touchend", function (evt) {
+                    if (konami.iphone.tap == true) konami.iphone.check_direction(link);
+                }, false);
+                konami.addEvent(document, "touchstart", function (evt) {
+                    konami.iphone.start_x = evt.changedTouches[0].pageX;
+                    konami.iphone.start_y = evt.changedTouches[0].pageY;
+                    konami.iphone.tap = true;
+                    konami.iphone.capture = true;
+                });
+            },
+            check_direction: function (link) {
+                x_magnitude = Math.abs(this.start_x - this.stop_x);
+                y_magnitude = Math.abs(this.start_y - this.stop_y);
+                x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
+                y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
+                result = (x_magnitude > y_magnitude) ? x : y;
+                result = (this.tap == true) ? "TAP" : result;
+
+                if (result == this.keys[0]) this.keys = this.keys.slice(1, this.keys.length);
+                if (this.keys.length == 0) {
+                    this.keys = this.orig_keys;
+                    this.code(link);
+                }
+            }
+        }
     }
 
-    var nodeinternet = document.getElementsByClassName("usointernet");
-    var usointernet = document.getElementById("usointernetlist").value;
-    var elinternet = document.createElement("div");
-    elinternet.setAttribute('class', 'classeusointernet');
-    var contentinternet = document.createTextNode("Usou a Internet");
-    elinternet.appendChild(contentinternet);
+    typeof callback === "string" && konami.load(callback);
+    if (typeof callback === "function") {
+        konami.code = callback;
+        konami.load();
+    }
 
-    var showinternet = document.getElementById("ano" + usointernet);
-    showinternet.insertBefore(elinternet, showinternet.childNodes[0]);
+    return konami;
+};
 
-    var usoweb = document.getElementById("usoweblist").value;
-    var elweb = document.createElement("div");
-    elweb.setAttribute('class', 'classeusoweb');
-    var contentweb= document.createTextNode("Usou a Web");
-    elweb.appendChild(contentweb);
-
-    var showweb = document.getElementById("ano" + usoweb);
-    showweb.insertBefore(elweb, showweb.childNodes[0]);
-
-    var usoimagem = document.getElementById("imagemnaweblist").value;
-    var elimagem = document.createElement("div");
-    elimagem.setAttribute('class', 'classusoimagem');
-    var contentimagem= document.createTextNode("Imagem na Web");
-    elimagem.appendChild(contentimagem);
-
-    var showimagem = document.getElementById("ano" + usoimagem);
-    showimagem.insertBefore(elimagem, showimagem.childNodes[0]);
-
-    var usovideo = document.getElementById("videonaweblist").value;
-    var elvideo = document.createElement("div");
-    elvideo.setAttribute('class', 'classusovideo');
-    var contentvideo= document.createTextNode("Vídeo na Web");
-    elvideo.appendChild(contentvideo);
-
-    var showvideo = document.getElementById("ano" + usovideo);
-    showvideo.insertBefore(elvideo, showvideo.childNodes[0]);
-
-    var usocompra = document.getElementById("compranaweblist").value;
-    var elcompra = document.createElement("div");
-    elcompra.setAttribute('class', 'classusocompra');
-    var contentcompra= document.createTextNode("Compra na Web");
-    elcompra.appendChild(contentcompra);
-
-    var showcompra = document.getElementById("ano" + usocompra);
-    showcompra.insertBefore(elcompra, showcompra.childNodes[0]);
-
-    var usobusca = document.getElementById("buscanaweblist").value;
-    var elbusca = document.createElement("div");
-    elbusca.setAttribute('class', 'classusobusca');
-    var contentbusca= document.createTextNode("Busca na Web");
-    elbusca.appendChild(contentbusca);
-
-    var showbusca = document.getElementById("ano" + usobusca);
-    showbusca.insertBefore(elbusca, showbusca.childNodes[0]);
-
-    var usorede = document.getElementById("redenaweblist").value;
-    var elrede = document.createElement("div");
-    elrede.setAttribute('class', 'classusorede');
-    var contentrede= document.createTextNode("Rede social");
-    elrede.appendChild(contentrede);
-
-    var showrede = document.getElementById("ano" + usorede);
-    showrede.insertBefore(elrede, showrede.childNodes[0]);
-
-    var hideall = document.getElementById("anoempty");
-    hideall.style.display="none";
-
-}
+var rei = document.getElementById("web-2009-ee");
+var idrei = document.getElementById("web-2009-ee-sp");
+var contentrei= document.createTextNode("Aguarde instruções");
+var easter_egg = new Konami(function() { 
+    rei.style.display = "inline-block";
+    idrei.appendChild(contentrei);
+});
