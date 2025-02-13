a9345a538008b76f03110f71cfdfa1ab7b7e2de8
Reinaldo Ferraz <reinaldo@nic.br>
2025-02-13 13:04:06 -0300

    Updating 2022-2025

---
+++ sw.js
@@ -0,0 +1,39 @@
+var CACHE_NAME = 'linhadotempo';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+          '/linhadotempo/',
+          '/linhadotempo/index.html',
+          '/linhadotempo/estiloh.css',
+          '/linhadotempo/script.js',
+          '/linhadotempo/single.css',
+          '/linhadotempo/BerkshireSwash-Regular.ttf',
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
