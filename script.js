378c79bf582490fd41bdabfe3f9fa4b3164785a3
Reinaldo Ferraz <reinaldo@nic.br>
2025-03-31 08:16:58 -0300

    ajustes no responsivo

--- script.js
+++ script.js
@@ -1,70 +1,56 @@
-//abre tooltip de cada elemento. Fecha os demais
+// Abre tooltip de cada elemento e fecha os demais
 function abre(event) {
-    var close = document.getElementsByClassName("hide");
-    var hide;
-    for (hide = 0; hide < close.length; hide++) {
-        close[hide].style.display = "none";
-    }
-    event.currentTarget.nextElementSibling.style.display = "inline";
-    event.currentTarget.nextElementSibling.setAttribute('aria-live', 'polite');
-    event.currentTarget.nextElementSibling.setAttribute('aria-atomic', 'true');
+  document.querySelectorAll(".hide").forEach((el) => {
+    el.style.display = "none";
+  });
+
+  const tooltip = event.currentTarget.nextElementSibling;
+  tooltip.style.display = "inline";
+  tooltip.setAttribute("aria-live", "polite");
+  tooltip.setAttribute("aria-atomic", "true");
 }
 
-
-//Exibe CSS mais simples
+// Troca a folha de estilo ativa
 function css(cssFile, cssLinkIndex) {
-        var horizontal = document.getElementsByTagName("link").item(cssLinkIndex);
-        var vertical = document.createElement("link");
-        vertical.setAttribute("rel", "stylesheet");
-        vertical.setAttribute("type", "text/css");
-        vertical.setAttribute("href", cssFile);
-        document.getElementsByTagName("head").item(0).replaceChild(vertical, horizontal);
-      }
+  const oldLink = document.getElementsByTagName("link")[cssLinkIndex];
+  const newLink = document.createElement("link");
 
-//fecha todos os elementos com esc
-document.onkeydown = function(e){
-    var close = document.getElementsByClassName("hide");
-    var hide;
-    if(e.keyCode === 27){
-        for (hide = 0; hide < close.length; hide++) {
-        close[hide].style.display = "none";
-        close[hide].setAttribute('aria-live', 'off');
-        close[hide].setAttribute('aria-atomic', 'false');
-    }
-    }
-};
+  newLink.rel = "stylesheet";
+  newLink.type = "text/css";
+  newLink.href = cssFile;
 
-//fecha todos os elementos com clique
+  document.head.replaceChild(newLink, oldLink);
+}
+
+// Fecha todos os tooltips ao pressionar ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    document.querySelectorAll(".hide").forEach((el) => {
+      el.style.display = "none";
+      el.setAttribute("aria-live", "off");
+      el.setAttribute("aria-atomic", "false");
+    });
+  }
+});
+
+// Fecha todos os elementos ao clicar fora
 function fecha() {
-    var close = document.getElementsByClassName("hide");
-    var hide;
-    for (hide = 0; hide < close.length; hide++) {
-        close[hide].style.display = "none";
-        close[hide].setAttribute('aria-live', 'off');
-    }
+  document.querySelectorAll(".hide").forEach((el) => {
+    el.style.display = "none";
+    el.setAttribute("aria-live", "off");
+  });
 }
 
-//acao do checkbox para abrir os eventos relacionados ao tema
+// Ação do checkbox para abrir os eventos relacionados ao tema
 function show() {
-    var web = document.getElementsByClassName("web");
-    var internet = document.getElementsByClassName("internet"); 
-    var checkweb = document.getElementById("checkweb");
-    var checkint = document.getElementById("checkint");
-    
-var allweb;
-    for (allweb = 0; allweb < web.length; allweb++){
-    if (checkweb.checked == 1){
-        web[allweb].style.display = 'block';
-    }else{
-         web[allweb].style.display = 'none';
-    }
-}
-var allinternet;
-    for (allinternet = 0; allinternet < internet.length; allinternet++){
-    if (checkint.checked == 1){
-        internet[allinternet].style.display = 'block';
-    }else{
-         internet[allinternet].style.display = 'none';
-    }
-}
+  const checkweb = document.getElementById("checkweb");
+  const checkint = document.getElementById("checkint");
+
+  document.querySelectorAll(".web").forEach((el) => {
+    el.style.display = checkweb.checked ? "block" : "none";
+  });
+
+  document.querySelectorAll(".internet").forEach((el) => {
+    el.style.display = checkint.checked ? "block" : "none";
+  });
 }
